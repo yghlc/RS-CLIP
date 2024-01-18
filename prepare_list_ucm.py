9df6048a8323d12bccf3c9a7bb073ffa18b7abc9
yghlc <hlchuang@126.com>
2024-01-18 12:44:09 +0800

    add

---
+++ prepare_list_ucm.py
@@ -0,0 +1,31 @@
+import os, glob
+all_list = []
+all_label = []
+
+# data_root = 'NWPU-RESISC45/'
+data_root = os.path.expanduser('~/Data/image_classification/UCMerced_LandUse/')
+
+all_classes = os.listdir(data_root + 'Images/')
+print(all_classes)
+
+f1 = open(data_root+'all.txt', 'w')
+f2 = open(data_root+'label_list.txt', 'w')
+
+for i, cls in enumerate(all_classes):
+    print(cls, i)
+    f2.writelines(cls + ', ' + str(i) + '\n')
+
+    # cls_list = glob.glob(os.path.join(data_root, cls, '*.jpg'))
+    # print(os.path.join(data_root, 'Images', cls,'*.tif'))
+    cls_list = glob.glob(os.path.join(data_root,'Images', cls, '*.tif'))
+    # print(cls_list)
+    # cls_list = ["/".join(dd.split('/')[1:]) for dd in cls_list]
+    cls_list = ["/".join(dd.split('/')[-2:]) for dd in cls_list]
+
+    for cc in cls_list:
+        f1.writelines(cc + ' ' + str(i) + '\n')
+
+    # break
+
+f1.close()
+f2.close()
