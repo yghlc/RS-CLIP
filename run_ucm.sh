cd9fc24d8b04ffbea11cff15af062682e0f31e0d
Lingcao Huang <huanglingcao@gmail.com>
2024-01-26 19:05:56 +0800

    better organize

--- run_ucm.sh
+++ run_ucm.sh
@@ -2,23 +2,26 @@
 #model=ViT-L/14
 model=RN50
 
-### UCM
+dataset=ucm
+
+## UCM
 # round 1
-# $(which python) generate_pseudo.py --dataset ucm --vis-model ${model} --pseudo-version 1 --topk 20
-#$(which python) generate_pseudo.py --dataset ucm --vis-model ${model} --pseudo-version 1 --topk 30
+k=20
+wdir1=work_dir_ucm_ViTL_pseudo_k${k}
+python generate_pseudo.py --dataset ${dataset} --vis-model ${model} --pseudo-version 1 --topk ${k}
+python train_zero.py --dataset ${dataset} --vis-model ${model} --n-shot ${k} --work-dir ${wdir1} --pseudo-version 1
 
-#$(which python) train_zero.py --dataset ucm --vis-model ${model} --n-shot 30 --work-dir work_dir_ucm_ViTL_pseudo_k30 --pseudo-version 1
-# $(which python) train_zero.py --dataset ucm --vis-model ${model} --n-shot 20 --work-dir work_dir_ucm_ViTL_pseudo_k30 --pseudo-version 1
 
+ # round 2
+k=40
+python generate_pseudo.py --dataset ${dataset} --vis-model ${model} --pseudo-version 2 --load-from ${wdir1}/batch_300.ckpt --topk ${k}
+wdir2=work_dir_ucm_ViTL_pseudo_k${k}
+python train_zero.py --dataset ${dataset} --vis-model ${model} --n-shot ${k} --work-dir ${wdir2} --load-from ${wdir1}/batch_300.ckpt --pseudo-version 2
 
-# # round 2
-# $(which python) generate_pseudo.py --dataset ucm --vis-model ${model} --pseudo-version 2 --load-from work_dir_ucm_ViTL_pseudo_k20/batch_300.ckpt --topk 30
-
-# $(which python) train_zero.py --dataset ucm --vis-model ${model} --n-shot 40 --work-dir work_dir_ucm_ViTL_pseudo_k40 --load-from work_dir_ucm_ViTL_pseudo_k20/batch_300.ckpt --pseudo-version 2
-
-# # round 3
-# $(which python) generate_pseudo.py --dataset ucm --vis-model ${model} --pseudo-version 3 --load-from work_dir_ucm_ViTL_pseudo_k40/batch_300.ckpt --topk 60
-
-# $(which python) train_zero.py --dataset ucm --vis-model ${model} --n-shot 70 --work-dir work_dir_ucm_ViTL_pseudo_k70 --load-from work_dir_ucm_ViTL_pseudo_k40/batch_300.ckpt --pseudo-version 3
+ # round 3
+k=60
+python generate_pseudo.py --dataset ${dataset} --vis-model ${model} --pseudo-version 3 --load-from ${wdir2}/batch_300.ckpt --topk 60
+wdir3=work_dir_ucm_ViTL_pseudo_k${k}
+python train_zero.py --dataset ${dataset} --vis-model ${model} --n-shot ${k} --work-dir ${wdir3} --load-from ${wdir2}/batch_300.ckpt --pseudo-version 3
 
 
